e7726c914d0fa1ee4f7a73e90003a65d872398e3
Darren Moriarty <dmoriarty@fexco.com>
2022-10-26 16:30:13 +0100

    formatted

--- libs/core/model/src/lib/company.ts
+++ libs/core/model/src/lib/company.ts
@@ -1,19 +1,17 @@
-
-
 export interface ICompany {
-    name: string;
-    address: {
-        addressOne: string;
-        addressTwo: string;
-        addressThree: string;
-        postcode: string;
-        country: string;
-        county: string;
-    };
-    users: [
-        {
-            name: string;
-            sex: string;
-        }
-    ];
+  name: string;
+  address: {
+    addressOne: string;
+    addressTwo: string;
+    addressThree: string;
+    postcode: string;
+    country: string;
+    county: string;
+  };
+  users: [
+    {
+      name: string;
+      sex: string;
+    }
+  ];
 }
